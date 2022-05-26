e0bd5a2c592fb64e9664f66753daaf4f90cd303a
Prajjwalgupta <Prajjwalg786@gmail.com>
2022-05-26 21:58:21 +0530

    Initial Commit

---
+++ Shared/TImeLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TImeLineApp.swift
+//  Shared
+//
+//  Created by Prajjawal Gupta on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TImeLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
